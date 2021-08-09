b18a16b6c24b7e18942ba75094e6894b4e3b9208
link4rkv <link4rkv@gmail.com>
2021-08-09 15:29:14 +0530

    added linters

--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 const baseURL = process.env.REACT_APP_API_BASE_URL;
 
 const axiosInstance = axios.create({
-  baseURL: baseURL
+  baseURL: baseURL,
 });
 
 export default axiosInstance;
