2dcf706bfc606fd8bb3ea076096f5466deb4e023
link4rkv <link4rkv@gmail.com>
2021-08-09 15:46:38 +0530

    8105 | Rahul | testing linters and add eslint config file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch } from "react-router-dom";
 
-import routes from './routes/index';
-import Layout from './components/layout/Layout';
+import routes from "./routes/index";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
