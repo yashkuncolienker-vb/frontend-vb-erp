682e86e02a24d235d3a0965c195627a4b9f8e5ec
yashkuncolienker-vb <89966303+yashkuncolienker-vb@users.noreply.github.com>
2021-11-17 13:17:02 +0530

    YASH RK|Styling for Navbar

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,17 +1,63 @@
+import { Box } from "@mui/system";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import vbLogo from "../../assets/images/vb_logo.svg";
+import IconButton from "@mui/material/IconButton";
+import Toolbar from "@mui/material/Toolbar";
+import AppBar from "@mui/material/AppBar";
+import { useState } from "react";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+
 const customStyles = {
-  backgroundColor: "cyan",
-  height: "70px",
-  position: "fixed",
-  width: "100%",
+  root: {
+    height: "70px",
+    position: "fixed",
+    width: "100%",
+  },
+  image: {
+    width: "180px",
+  },
+  toolbar: {
+    display: "flex",
+    height: "70px",
+    justifyContent: "space-between",
+    margin: "0 30px",
+  },
+  appbar: {
+    backgroundColor: "white",
+  },
 };
 
 const Header = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
-    <header style={customStyles}>
-      <div>CMS</div>
-      <nav></nav>
-    </header>
+    <Box sx={customStyles.root}>
+      <AppBar sx={customStyles.appbar} position="static" elevation={4}>
+        <Toolbar sx={customStyles.toolbar}>
+          <img style={customStyles.image} src={vbLogo} alt="vb-logo" />
+          <IconButton size="large" edge="end" onClick={handleClick}>
+            <AccountCircle fontSize="large" />
+          </IconButton>
+          <Menu
+            id="profile-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>Profile</MenuItem>
+            <MenuItem onClick={handleClose}>My account</MenuItem>
+            <MenuItem onClick={handleClose}>Logout</MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
-
 export default Header;
