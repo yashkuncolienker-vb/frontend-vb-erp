310536b7b586089746e0327f86aa93270083652d
yashkuncolienker-vb <89966303+yashkuncolienker-vb@users.noreply.github.com>
2021-11-19 16:59:31 +0530

    YASH RK||project structure

---
+++ src/components/employees/EmployeesList.js
@@ -0,0 +1,13 @@
+const EmployeesList = () => {
+  return (
+    <div>
+      <ul>
+        <li>Employees1</li>
+        <li>Employees2</li>
+        <li>Employees3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default EmployeesList;
---
+++ src/pages/Employees/Employees.js
@@ -0,0 +1,7 @@
+import EmployeesList from "../../components/employees/EmployeesList";
+
+const Employees = () => {
+  return <EmployeesList />;
+};
+
+export default Employees;
